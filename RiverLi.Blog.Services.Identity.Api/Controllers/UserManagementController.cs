3e38f0cb99472220fe603e29580022e7f0827217
liguanwen <910082701@qq.com>
2026-05-18 10:55:44 +0800

    refactor(user): 添加必要的命名空间导入以支持异步操作
    - 添加 System 命名空间导入
    - 添加 System.Collections.Generic 命名空间导入
    - 添加 System.Threading.Tasks 命名空间导入
    - 为控制器提供完整的异步操作支持
    - 确保代码依赖项的一致性

--- RiverLi.Blog.Services.Identity.Api/Controllers/UserManagementController.cs
+++ RiverLi.Blog.Services.Identity.Api/Controllers/UserManagementController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using RiverLi.Blog.Identity.Application.Users.Commands;
